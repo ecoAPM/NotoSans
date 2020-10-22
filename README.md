4cba9801d20767c8f4f9c978dd38d0f16da7ab04
Steve Desmond <steve@vtsv.ca>
2020-10-22 09:56:14 -0400

    Automate build process

---
+++ README.md
@@ -0,0 +1,3 @@
+# Noto Sans Web Fonts
+
+An automated process to download and convert Google's Noto Sans font
---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ! -f NotoSans-hinted.zip ]
+then
+  wget https://noto-website-2.storage.googleapis.com/pkgs/NotoSans-hinted.zip
+fi
+
+unzip -o NotoSans-hinted.zip -d src
+rm src/*Condensed*.ttf
+rm src/*Italic*.ttf
+
+mkdir -p dist
+ls src/*.ttf | xargs -I x woff2_compress x
+mv src/*.woff2 dist
+cp src/LICENSE_OFL.txt src/README dist
